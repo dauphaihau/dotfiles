82dc7f07c7f3d22d77a160adaebd36419a24760f
hau <dauphaihau@gmail.com>
2026-03-25 13:38:45 +0700

    auto backup: 2026-03-25 13:38:45

--- zsh/.aliases/git.sh
+++ zsh/.aliases/git.sh
@@ -16,3 +16,10 @@ gwip() { git add -A && git commit -m "WIP"; }
 ghprc() { [[ -z "$1" ]] && echo "Usage: ghprc <target-branch>" && return 1; gh pr create --base "$1"; }
 gclean() { git fetch -p && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d; }
 gfetch() { [[ -z "$1" ]] && echo "Usage: gfetch <branch>" && return 1; git fetch origin "$1"; }
+
+gpull() { [[ -z "$1" ]] && echo "Usage: gpull <branch>" && return 1; git fetch origin "$1:$1"; }
+gpush() { [[ -z "$1" ]] && echo "Usage: gpush <branch>" && return 1; git push origin "$1"; }
+gdb() { [[ -z "$1" ]] && echo "Usage: gdb <branch>" && return 1; git diff "$1"..HEAD --word-diff; }
+glb() { [[ -z "$1" ]] && echo "Usage: glb <branch>" && return 1; git log --oneline --graph --decorate "$1"; }
+gmb() { [[ -z "$1" ]] && echo "Usage: gmb <branch>" && return 1; git merge "$1" --no-ff; }
+grb() { [[ -z "$1" ]] && echo "Usage: grb <branch>" && return 1; git rebase "$1"; }
