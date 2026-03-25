c7edac95021c5019147e11964cefeba61b7bcf75
hau <dauphaihau@gmail.com>
2026-03-25 09:06:51 +0700

    auto backup: 2026-03-25 09:06:51

--- zsh/.aliases/git.sh
+++ zsh/.aliases/git.sh
@@ -10,15 +10,9 @@ alias gcb='git checkout -b'
 alias gpub='git push -u origin HEAD'
 alias gsync='git pull --rebase --prune'
 alias gp='git pull'
+
 gundo() { git reset --soft HEAD~${1:-1}; }
 gwip() { git add -A && git commit -m "WIP"; }
 ghprc() { [[ -z "$1" ]] && echo "Usage: ghprc <target-branch>" && return 1; gh pr create --base "$1"; }
-# gbl() {
-#   local path="."
-#   for arg in "$@"; do
-#     [[ "$arg" == --rp=* ]] && path="${arg#--rp=}"
-#   done
-#   git -C "$path" branch -a
-# }
 gclean() { git fetch -p && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d; }
 gfetch() { [[ -z "$1" ]] && echo "Usage: gfetch <branch>" && return 1; git fetch origin "$1"; }
