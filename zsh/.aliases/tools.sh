4bb0b2074c0d4c6868186d917515383faada8335
hau <dauphaihau@gmail.com>
2026-04-10 09:20:50 +0700

    auto backup: 2026-04-10 09:20:50

--- zsh/.aliases/tools.sh
+++ zsh/.aliases/tools.sh
@@ -3,7 +3,7 @@ alias rl='unalias -a && for f in ~/.aliases/*.sh; do [ -f "$f" ] && source "$f";
 alias reapply-icons='sudo bash /Volumes/Local/dev/pj-personal/dotfiles/scripts/reapply-icons.sh'
 
 alias c='codex'
-alias cl='claude'
+alias cc='claude'
 alias cs='cursor'
 alias z='zed'
 alias j='just'
