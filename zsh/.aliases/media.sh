eb02c2f21369806e4c7eeb15bf0ecb163008eeca
hau <dauphaihau@gmail.com>
2026-04-11 14:28:34 +0700

    auto backup: 2026-04-11 14:28:34

--- zsh/.aliases/media.sh
+++ zsh/.aliases/media.sh
@@ -4,6 +4,23 @@ dlm() { yt-dlp -x --audio-format mp3 "$1" }
 # download video: download video as mp4 (best h264 quality) + embed caption as Finder comment
 dlv() {
   local url="$1"
+  shift
+  local range=""
+  local -a extra_args=()
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --range)
+        range="$2"
+        shift 2
+        ;;
+      *)
+        extra_args+=("$1")
+        shift
+        ;;
+    esac
+  done
+
   local desc
   desc=$(yt-dlp \
     --print "%(description)s" \
@@ -19,6 +36,9 @@ dlv() {
     -o "%(title).100B [%(id)s].%(ext)s"         # truncate title to 100 bytes to avoid long filename errors
     --print after_move:filepath                 # print final filepath after download+merge
   )
+
+  [[ -n "$range" ]] && args+=(--download-sections "$range")
+  args+=("${extra_args[@]}")
 
   yt-dlp "${args[@]}" "$url" | while read -r filepath; do
       if [[ -n "$desc" ]]; then
