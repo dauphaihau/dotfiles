8802ba7657792cc0cdd62768f5b106096ffd156b
hau <dauphaihau@gmail.com>
2026-07-02 00:06:34 +0700

    auto backup: 2026-07-02 00:06:34

--- zsh/.aliases/media.sh
+++ zsh/.aliases/media.sh
@@ -137,15 +137,38 @@ dlv() {
 dlig() {
   local url="$1"
   shift
+  local -a extra_args=()
+  local -a auth_args=()
+  local has_cookie_args=0
 
   if ! command -v gallery-dl >/dev/null 2>&1; then
     echo "gallery-dl is not installed. Install it with: brew install gallery-dl" >&2
     return 127
   fi
 
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --cookies|--cookies-from-browser)
+        has_cookie_args=1
+        extra_args+=("$1")
+        [[ -n "$2" ]] && extra_args+=("$2")
+        shift 2
+        ;;
+      *)
+        extra_args+=("$1")
+        shift
+        ;;
+    esac
+  done
+
+  if [[ "$url" == *"instagram.com"* && $has_cookie_args -eq 0 ]]; then
+    auth_args+=(--cookies-from-browser firefox)
+  fi
+
   gallery-dl \
     -D . \
-    "$@" \
+    "${auth_args[@]}" \
+    "${extra_args[@]}" \
     "$url"
 }
 
