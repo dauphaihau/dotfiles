a703e049ea092fdff3026c85c09a5ac310d0de04
hau <dauphaihau@gmail.com>
2026-08-19 19:04:52 +0700

    auto backup: 2026-08-19 19:04:52

--- zsh/.aliases/package-managers.sh
+++ zsh/.aliases/package-managers.sh
@@ -3,6 +3,15 @@ alias bru='brew uninstall'
 alias brl='brew list'
 alias broc='brew outdated'
 
+# nr: node runtime
+alias nr='volta'              # node runtime manager
+alias nra='volta list'        # list all managed tools
+alias nrl='volta list node'   # list node versions
+alias nri='volta install'     # install/set global default, e.g. nri node@22
+alias nru='volta uninstall'   # remove a node version, e.g. nru node@20
+alias nrp='volta pin'         # pin/swap project version, e.g. nrp node@22
+alias nrw='volta which'       # show active tool path, e.g. nrw node
+
 alias ppd='pnpm dev'
 alias ppa='pnpm add'
 alias ppad='pnpm add -D'
